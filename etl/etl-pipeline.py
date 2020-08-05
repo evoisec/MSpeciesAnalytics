bb551eb1ef968f5d517bd431fceae163047e886b
evo <evo.eftimov@isecc.com>
2020-08-05 17:24:29 +0100

    ws

--- etl/etl-pipeline.py
+++ etl/etl-pipeline.py
@@ -4,7 +4,31 @@ import numpy as np
 
 #Note: the ref data dictionories can be initialized/loaded from file/db for increased config flexibility
 translDictCapShape = {'b': 'bell', 'c': 'conical', 'x': 'convex','f': 'flat', 'k': 'knobbed', 's': 'sunken'}
+translDictCapColor = {'n': 'brown', 'b': 'buff', 'c': 'cinnamon', 'g': 'gray', 'r': 'green', 'p': 'pink', 'u': 'purple', 'e': 'red', 'w': 'white', 'y': 'yellow'}
+translDictOdor = {'a': 'almond', 'l': 'anise', 'c': 'creosote', 'y': 'fishy', 'f': 'foul', 'm': 'musty', 'n': 'none', 'p': 'pungent', 's': 'spicy'}
+translDictGillSize = {'b': 'broad', 'n': 'narrow'}
+translDictGillColor = {'k': 'black', 'n': 'brown', 'b': 'buff', 'h': 'chocolate', 'g': 'gray', 'r': 'green', 'o': 'orange', 'p': 'pink', 'u': 'purple', 'e': 'red',
+                       'w': 'white', 'y': 'yellow'}
+translDictstalkColorAboveRing = {'n': 'brown', 'b': 'buff', 'c': 'cinnamon', 'g': 'gray', 'o': 'orange', 'p': 'pink', 'e': 'red', 'w': 'white', 'y': 'yellow'}
+translDictVeilColor = {'n': 'brown', 'o': 'orange', 'w': 'white', 'y': 'yellow'}
+translDictRingType = {'c': 'cobwebby', 'e': 'evanescent', 'f': 'flaring', 'l': 'large', 'n': 'none', 'p': 'pendant', 's': 'sheathing', 'z': 'zone'}
+translDictSporePrintColor = {'k': 'black', 'n': 'brown', 'b': 'buff', 'h': 'chocolate', 'r': 'green', 'o': 'orange', 'u': 'purple', 'w': 'white', 'y': 'yellow'}
+translDictPopulation = {'a': 'abundant', 'c': 'clustered', 'n': 'numerous', 's': 'scattered', 'v': 'several', 'y': 'solitary'}
+translDictHabitat = {'g': 'grasses', 'l': 'leaves', 'm': 'meadows', 'p': 'paths', 'u': 'urban', 'w': 'waste', 'd': 'woods'}
+
+
 columnDict = {'cap_shape': translDictCapShape}
+columnDict['cap_color'] = translDictCapColor
+columnDict['odor'] = translDictOdor
+columnDict['gill_size'] = translDictGillSize
+columnDict['gill_color'] = translDictGillColor
+columnDict['stalk_color_above_ring'] = translDictstalkColorAboveRing
+columnDict['veil_color'] = translDictVeilColor
+columnDict['ring_type'] = translDictRingType
+columnDict['spore_print_color'] = translDictSporePrintColor
+columnDict['population'] = translDictPopulation
+columnDict['habitat'] = translDictHabitat
+
 
 # Pluggable (in any ETL workflow step) Data Engine for Validates and Translates of Data according to specific algo
 def dataEngineVT(x, dict):
@@ -40,10 +64,21 @@ for colName in mushroomsPDF.columns:
     print(colName)
 
     # validates and translates only one column so far, on purpose
-    if colName == 'cap_shape':
+    if colName != 'lat' and colName != 'lon' and colName != 'Time':
         mushroomsPDF[colName] = mushroomsPDF[colName].apply(dataEngineVT, dict=columnDict.get(colName))
 
+# Conver the Python None to the Pandas NaN
+mushroomsPDF = mushroomsPDF.fillna(value=np.nan)
+
 print(mushroomsPDF.head(5))
+print(mushroomsPDF)
+
+
+#################################################
+# Persist to file dataset
+#################################################
+
+mushroomsPDF.to_csv("c:\\Users\evo\mushrooms-processed.csv", index = False, header=True)
 
 #################################################
 # Bulk Insert in e.g. PostgressSQL DB
